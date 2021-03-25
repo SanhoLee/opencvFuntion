72ec05e9a3b2a5976f34d4c0a8fc7e49ef7fe0ba
SanhoLee <stlsh0516@gmail.com>
2021-03-25 22:50:23 +0900

    HoughLine Function : retype the loop contents

--- HoughLines_.cpp
+++ HoughLines_.cpp
@@ -64,27 +64,49 @@ int main()
     double theta_thres = (CV_PI / 180);
     int ptr_votes_thres = 20;
 
-    HoughLines(imgEdge, lines, rho_thres, theta_thres, ptr_votes_thres, 0, 0);
+    HoughLines(imgEdge, lines, rho_thres, theta_thres, ptr_votes_thres);
 
+    cout << "\t-----check picture spec. -----\t" << endl;
+    cout << "img pixels --> width : " << imgEdge.cols << ", height : " << imgEdge.rows << endl;
     cout << "lines : " << lines.size() << endl;
 
     // need to studying this part...
-    for (size_t i = 0; i < lines.size(); i++)
+    for (int i = 0; i < lines.size(); i++)
     {
-        float rho = lines[i][0], theta = lines[i][1];
+        // 다시 써보기.
+        double rho = lines[i][0], theta = lines[i][1];
+        double a = cos(theta);
+        double b = sin(theta);
+
+        // 직선 위의 한점을 (x0,y0) 로 정의
+        double x0 = rho * a, y0 = rho * b;
+        // 직선의 길이를 나타내기 위해, 충분히 큰 값으로 지정한다.
+        // 직선의 픽셀 길이 값이 되기 때문에, 이미지 픽셀의 크기에 따라 조절해주면 된다.
+        double dL = 1000;
+
+        // 직선을 표현하기 위해 포인트 변수 생성.
         Point pt1, pt2;
-        double a = cos(theta), b = sin(theta);
-        double x0 = a * rho, y0 = b * rho;
-        pt1.x = cvRound(x0 + 1000 * (-b));
-        pt1.y = cvRound(y0 + 1000 * (a));
-        pt2.x = cvRound(x0 - 1000 * (-b));
-        pt2.y = cvRound(y0 - 1000 * (a));
-        line(img, pt1, pt2, Scalar(0, 0, 255), 3, LINE_AA);
-        cout << "line " << i + 1 << "->> rho : " << lines[i][0] << "px, \t"
-             << " theta : " << lines[i][1] * (180 / CV_PI) << endl;
+
+        // pt1, pt2의 좌표를 가져오기 위해 동일 직선 위를 움직이는 변위 변수 dL과 cos, sin 함수의 조합을 활용했다.
+        pt1.x = x0 - dL * b;
+        pt1.y = y0 + dL * a;
+        pt2.x = x0 + dL * b;
+        pt2.y = y0 - dL * a;
+
+        // 검출된 라인의 속성 값(rho, theta)과 해당 직선의 포인트 값을 출력.
+        cout << "line " << i + 1
+             << "->> rho : " << lines[i][0] << "px, \t"
+             << " theta : " << lines[i][1] * (180 / CV_PI)
+             << "\t Point1(x,y) : " << pt1.x << "\t ," << pt1.y
+             << "\t Point2(x,y) : " << pt2.x << "\t ," << pt2.y
+             << endl;
+
+        // 원본 이미지에 검출된 직선을 그림.
+        line(img, pt1, pt2, Scalar(0, 0, 200), 3, LINE_AA);
     }
 
     imshow("img", img);
+    imwrite("data/output/HoughLines_ini.png", img);
     waitKey(0);
 
     return 0;
