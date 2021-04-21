d7850ec59cd7e07d93f09035e89c4ac5c2c22c25
SanhoLee <stlsh0516@gmail.com>
2021-04-21 23:17:14 +0900

    Done fillPoly function.

--- main.cpp
+++ main.cpp
@@ -23,27 +23,37 @@ int main(int argc, char **argv)
     Mat img2(HEIGHT, WIDTH, CV_8UC3, Scalar(255, 255, 255));
 
     vector<vector<Point>> squre_point(2, vector<Point>(SQURE));
-    squre_point[0][0] = Point(WIDTH - GAP1, HEIGHT - GAP1);
-    squre_point[0][1] = Point(WIDTH - GAP1, GAP1);
-    squre_point[0][2] = Point(GAP1, GAP1);
-    squre_point[0][3] = Point(GAP1, HEIGHT - GAP1);
+
+    // #1 squre
+    squre_point[0][0] = Point(0, 0);
+    squre_point[0][1] = Point(GAP1, 0);
+    squre_point[0][2] = Point(GAP1, HEIGHT);
+    squre_point[0][3] = Point(0, HEIGHT);
+    // #2 squre
+    squre_point[1][0] = Point(0, 0);
+    squre_point[1][1] = Point(WIDTH, 0);
+    squre_point[1][2] = Point(WIDTH, GAP1);
+    squre_point[1][3] = Point(0, GAP1);
 
     /* Todos
 
-    .make different img with fillpoly. but imgs should be intersect on specific Area.
-    .make 2 polygon shape with vertice points.
+    [done].make different img with fillpoly. but imgs should be intersect on specific Area.
+    [done].make 2 polygon shape with vertice points.
     .do bitwise Calculation with two imgs.
 
     */
     fillPoly(img1, {squre_point[0]}, Scalar(255, 0, 0), LINE_AA);
+    fillPoly(img2, {squre_point[1]}, Scalar(255, 0, 0), LINE_AA);
 
     imshow("img1", img1);
     imshow("img2", img2);
-    imshow("zeros", zeros);
+    // imshow("zeros", zeros);
 
     rtnWaitKey = waitKey(0);
     if (rtnWaitKey == 27)
     {
+        imwrite("data/img1.jpg", img1);
+        imwrite("data/img2.jpg", img2);
         return 0;
     }
 }
